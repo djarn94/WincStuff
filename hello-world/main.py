a9af27da6b8e5f47d764b3b84f217345bd5d312f
djarn94 <superdjarn@gmail.com>
2023-05-17 13:08:35 +0200

    // new change

---
+++ hello-world/main.py
@@ -0,0 +1,7 @@
+# Do not modify these lines
+__winc_id__ = 'e75b6cd4a7404e3ca76c308566dafb5d'
+__human_name__ = 'hello-world'
+
+# Add your code after this line
+print("Hello, world")
+print("first change")
